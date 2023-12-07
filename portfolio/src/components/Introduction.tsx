0d084c50ffe6e08d27b1214786331126673529c5
Joel Kong <joelkong2001@gmail.com>
2023-12-07 20:53:17 +0800

    feat: created typewriter effect

--- portfolio/src/components/Introduction.tsx
+++ portfolio/src/components/Introduction.tsx
@@ -1,21 +1,58 @@
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Achievements from "./Achievements";
 
 export default function Introduction(): JSX.Element {
+  const [text, setText] = useState("");
+  const [showCursor, setShowCursor] = useState(true);
+
+  useEffect(() => {
+    const introText = "Hi, My Name is Joel Kong";
+    let index = 0;
+    let cursorInterval: any;
+
+    const typingEffect = () => {
+      if (index <= introText.length) {
+        setText(introText.substring(0, index));
+        index++;
+        setTimeout(typingEffect, 40);
+      } else {
+        cursorInterval = setInterval(() => {
+          setShowCursor((prev) => !prev);
+        }, 500);
+      }
+    };
+
+    typingEffect();
+
+    const intervalTimeout = setTimeout(() => {
+      clearInterval(cursorInterval);
+      setShowCursor(false);
+    }, 2000);
+
+    return () => {
+      clearTimeout(intervalTimeout);
+      clearInterval(cursorInterval);
+    };
+  }, []);
+
   return (
     <main className="max-h-screen overflow-y-scroll overflow-x-hidden snap-y snap-mandatory">
       <div className="pt-20 h-screen w-screen bg-gradient-to-r from-green-200 to-green-300 flex md:flex-row flex-col-reverse snap-center">
         <div className="w-full md:w-1/2 h-full flex justify-center items-center">
           <div className="w-3/4 h-3/4 flex flex-col pl-4 justify-center">
             <p className="text-5xl md:text-6xl tracking-wider leading-snug text-red-400 font-bold md:mt-0 mt-6">
-              Hi, My Name is <br></br> Joel Kong
+              {text}
+              <span className={showCursor ? "opacity-100" : "opacity-0"}>
+                |
+              </span>
             </p>
             <p className="mt-8 md:mt-10 text-xl font-semibold tracking-wide text-gray-500">
               Aspiring Software Engineer with a drive to make a difference
               through technology and make the world a better place
             </p>
-            <div className="mt-10 text-xl font-semibold tracking-wide flex flex-row w-full">
+            <div className="mt-10 md:mb-0 mb-10 text-xl font-semibold tracking-wide flex flex-row w-full">
               <Link
                 href="https://github.com/JoelKong"
                 target="_blank"
