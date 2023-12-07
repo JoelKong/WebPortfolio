ba5820a4e175c1e4c0eaa2eea4acc784780f1498
Joel Kong <joelkong2001@gmail.com>
2023-12-07 18:05:45 +0800

    feat: optimised intro for mobile

--- portfolio/src/components/Introduction.tsx
+++ portfolio/src/components/Introduction.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 export default function Introduction(): JSX.Element {
   return (
     <div className="pt-20 h-screen w-screen bg-gradient-to-r from-green-200 to-green-300 flex md:flex-row flex-col-reverse">
-      <div className="w-1/2 h-full flex justify-center items-center">
-        <div className="w-3/4 h-3/4 flex flex-col pl-4">
-          <p className="text-6xl tracking-wider leading-snug text-red-400 font-bold">
+      <div className="w-full md:w-1/2 h-full flex justify-center items-center">
+        <div className="w-3/4 h-3/4 flex flex-col pl-4 justify-center">
+          <p className="text-5xl md:text-6xl tracking-wider leading-snug text-red-400 font-bold md:mt-0 mt-6">
             Hi, My Name is <br></br> Joel Kong
           </p>
-          <p className="mt-10 text-xl font-semibold tracking-wide text-gray-500">
+          <p className="mt-8 md:mt-10 text-xl font-semibold tracking-wide text-gray-500">
             Aspiring Software Engineer with a drive to make a difference through
             technology and make the world a better place
           </p>
@@ -41,8 +41,8 @@ export default function Introduction(): JSX.Element {
           </div>
         </div>
       </div>
-      <div className="w-1/2 h-full flex justify-center items-center">
-        <div className="w-96 h-96 rounded-[50%] overflow-hidden">
+      <div className="w-full md:w-1/2 md:mt-0 mt-14 h-full flex justify-center items-center">
+        <div className="w-60 h-60 sm:w-80 sm:h-80 md:w-96 md:h-96 rounded-[50%] overflow-hidden">
           <Image
             className="w-full h-full object-cover"
             src={"/joelphoto.jpg"}
